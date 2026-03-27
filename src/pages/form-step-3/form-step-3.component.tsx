630217b6d23a0828357f5755082aa217274f3c13
matheus-dev-fs <matheusmks10@gmail.com>
2026-03-27 16:23:09 -0300

    feat: implement structure, styling, and logic for form step 3
    - create layout and styled components for the third step of the form
    - add controlled inputs for email and github with context integration
    - validate required and invalid email, and required github fields with warning messages
    - autofocus input fields on validation errors for better UX
    - add navigation guard to prevent access without previous steps completed
    - finalize form and log state on successful submission

--- src/pages/form-step-3/form-step-3.component.tsx
+++ src/pages/form-step-3/form-step-3.component.tsx
@@ -1,12 +1,158 @@
-import type { FC } from 'react';
+import { useEffect, useRef, useState, type ChangeEvent, type Dispatch, type FC, type RefObject, type SetStateAction } from 'react';
 import * as S from './form-step-3.styles';
 import { Theme } from '../../components/theme/theme.component';
+import { Link, useNavigate, type NavigateFunction } from 'react-router-dom';
+import { useForm } from '../../hooks/use-form.hook';
+import type { ContextType } from '../../types/form/context.type';
+import { FormActions } from '../../enums/form-actions.enum';
+import type { Action } from '../../types/form/action.type';
 
 export const FormStep3: FC = () => {
+    const navigate: NavigateFunction = useNavigate();
+    const { state, dispatch }: ContextType = useForm();
+
+    const [requiredEmailWarning, setRequiredEmailWarning]: [
+        boolean,
+        Dispatch<SetStateAction<boolean>>
+    ] = useState<boolean>(false);
+
+    const [requiredGithubWarning, setRequiredGithubWarning]: [
+        boolean,
+        Dispatch<SetStateAction<boolean>>
+    ] = useState<boolean>(false);
+
+    const [invalidEmailWarning, setInvalidEmailWarning]: [
+        boolean,
+        Dispatch<SetStateAction<boolean>>
+    ] = useState<boolean>(false);
+
+    const emailInputRef: RefObject<HTMLInputElement | null> = useRef<HTMLInputElement>(null);
+    const githubInputRef: RefObject<HTMLInputElement | null> = useRef<HTMLInputElement>(null);
+
+    useEffect((): void => {
+        if (state.name.trim() === '') {
+            navigate('/');
+            return;
+        }
+
+        dispatch({ type: FormActions.setCurrentStep, payload: 3 });
+    }, []);
+
+    const handleNextStep = (): void => {
+        const IS_EMAIL_EMPTY: boolean = state.email.trim() === '';
+
+        if (IS_EMAIL_EMPTY) {
+            setRequiredEmailWarning(true);
+            emailInputRef.current?.focus();
+            return;
+        }
+
+        const EMAIL_REGEX: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const IS_EMAIL_INVALID: boolean = !EMAIL_REGEX.test(state.email);
+
+        if (!invalidEmailWarning && IS_EMAIL_INVALID) {
+            setInvalidEmailWarning(true);
+            emailInputRef.current?.focus();
+            return;
+        }
+
+        const IS_GITHUB_EMPTY: boolean = state.github.trim() === '';
+
+        if (IS_GITHUB_EMPTY) {
+            setRequiredGithubWarning(true);
+            githubInputRef.current?.focus();
+            return;
+        }
+
+
+        console.log('Formulário finalizado com sucesso!');
+        console.log(state);
+    }
+
+    const resetWarningsAndDispatch = (
+        warnings: { warningState: boolean, setWarning: Dispatch<SetStateAction<boolean>> }[],
+        action: Action
+    ): void => {
+        warnings.forEach(({ warningState, setWarning }) => {
+            if (warningState) {
+                setWarning(false);
+            }
+        });
+
+        dispatch(action);
+    };
+
+    const handleEmailInputChange = (event: ChangeEvent<HTMLInputElement>): void => {
+        const value: string = event.target.value;
+
+        resetWarningsAndDispatch(
+            [
+                { warningState: requiredEmailWarning, setWarning: setRequiredEmailWarning },
+                { warningState: invalidEmailWarning, setWarning: setInvalidEmailWarning }
+            ],
+            { type: FormActions.setEmail, payload: value }
+        );
+    }
+
+    const handleGithubInputChange = (event: ChangeEvent<HTMLInputElement>): void => {
+        const value: string = event.target.value;
+
+        resetWarningsAndDispatch(
+            [
+                { warningState: requiredGithubWarning, setWarning: setRequiredGithubWarning }
+            ],
+            { type: FormActions.setGithub, payload: value }
+        );
+    }
+
     return (
         <Theme>
             <S.Container>
-                Passo 3
+                <S.Step>Passo 3/3</S.Step>
+                <S.Title>Legal, {state.name}! Onde te encontramos?</S.Title>
+                <S.Description>Preencha com seus contatos para que possamos te localizar</S.Description>
+
+                <S.Hr />
+
+                <S.Label>
+                    Qual seu email?
+                    <S.Input
+                        type="email"
+                        value={state.email}
+                        onChange={handleEmailInputChange}
+                        ref={emailInputRef}
+                    />
+                </S.Label>
+
+                {requiredEmailWarning &&
+                    <S.Warning aria-live="polite">O email é obrigatório
+                    </S.Warning>
+                }
+
+                {invalidEmailWarning &&
+                    <S.Warning aria-live="polite">O email é inválido
+                    </S.Warning>
+                }
+
+                <S.Label>
+                    Qual seu github?
+                    <S.Input
+                        type="text"
+                        value={state.github}
+                        onChange={handleGithubInputChange}
+                        ref={githubInputRef}
+                    />
+                </S.Label>
+
+                {requiredGithubWarning &&
+                    <S.Warning aria-live="polite">O github é obrigatório
+                    </S.Warning>
+                }
+
+
+                <Link to="/step-2" className='back-button'>Voltar</Link>
+                <S.Button onClick={handleNextStep}>Finalizar cadastro</S.Button>
+
             </S.Container>
         </Theme>
     );
--- src/pages/form-step-3/form-step-3.styles.ts
+++ src/pages/form-step-3/form-step-3.styles.ts
@@ -1,3 +1,70 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    .back-button {
+        font-size: 16px;
+        text-decoration: none;
+        padding: 20px 40px;
+        color: #B8B8D4;
+    }
+
+    .back-button:hover {
+        color: #FFF;
+    }
+`;
+
+export const Step = styled.p`
+    font-size: 13px;
+    color: #B8B8D4;
+`;
+
+export const Title = styled.h1`
+    font-size: 26px;
+    margin: 15px 0;
+`;
+
+export const Description = styled.p``;
+
+export const Hr = styled.hr`
+    height: 1px;
+    border: none;
+    background-color: #16195C;
+    margin: 30px 0;
+`;
+
+export const Label = styled.label`
+    display: block;
+    margin-top: 15px;
+    font-size: 13px;
+`;
+
+export const Input = styled.input`
+    display: block;
+    margin-top: 7px;
+    width: 100%;
+    padding: 20px 10px;
+    border: 2px solid #25CD89;
+    border-radius: 10px;
+    color: #FFF;
+    outline: none;
+    font-size: 15px;
+    background-color: transparent;
+`;
+
+export const Warning = styled.p`
+    font-size: 13px;
+    color: #FF0000;
+    margin-top: 7px;
+`;
+
+export const Button = styled.button`
+    background-color: #25CD89;
+    color: #FFF;
+    font-size: 14px;
+    font-weight: bold;
+    padding: 20px 40px;
+    border: none;
+    border-radius: 30px;
+    cursor: pointer;
+    margin-top: 30px;
+`;
